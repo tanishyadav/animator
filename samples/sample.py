19cd12b9e7acf7214b00436c1ecd2a0a971d6e73
Tanish Yadav <68607350+tanishyadav@users.noreply.github.com>
2020-10-19 13:39:04 +0530

    Create sample.py

---
+++ samples/sample.py
@@ -0,0 +1,5 @@
+from animator import contract, expand
+import os
+
+contract(os.get_terminal_size().columns, os.get_terminal_size().lines, color="\033[32m", "|", 0)
+expand(os.get_terminal_size().columns, os.get_terminal_size().lines, color="\033[32m", "|", 0)
