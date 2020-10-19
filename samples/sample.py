5ed9e433d4ca5bc09486f38ddaa44049798b2cd2
Tanish Yadav <68607350+tanishyadav@users.noreply.github.com>
2020-10-19 13:40:41 +0530

    Update sample.py

--- samples/sample.py
+++ samples/sample.py
@@ -1,5 +1,5 @@
 from animator import contract, expand
 import os
 
-contract(os.get_terminal_size().columns, os.get_terminal_size().lines, color="\033[32m", "|", 0)
-expand(os.get_terminal_size().columns, os.get_terminal_size().lines, color="\033[32m", "|", 0)
+contract(os.get_terminal_size().columns, os.get_terminal_size().lines, "\033[32m", "|", 0)
+expand(os.get_terminal_size().columns, os.get_terminal_size().lines, "\033[32m", "|", 0)
