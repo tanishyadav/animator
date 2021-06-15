c28ed7b4ad357307a5da978ff414204d6d9dc7ec
Tanish Yadav <tanish.io@outlook.com>
2021-06-15 12:57:22 +0530

    Add signature

--- samples/sample.py
+++ samples/sample.py
@@ -3,3 +3,13 @@ import os
 
 contract(os.get_terminal_size().columns, os.get_terminal_size().lines, "\033[32m", "|", 0)
 expand(os.get_terminal_size().columns, os.get_terminal_size().lines, "\033[32m", "|", 0)
+
+"""
+  ______            _      ____  __          __           
+ /_  __/___ _____  (_)____/ /\ \/ /___ _____/ /___ __   __
+  / / / __ `/ __ \/ / ___/ __ \  / __ `/ __  / __ `/ | / /
+ / / / /_/ / / / / (__  ) / / / / /_/ / /_/ / /_/ /| |/ / 
+/_/  \__,_/_/ /_/_/____/_/ /_/_/\__,_/\__,_/\__,_/ |___/  
+
+WAS HERE
+"""
